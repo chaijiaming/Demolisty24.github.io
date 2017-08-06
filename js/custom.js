abf17937b164e2d8c90919c71593e203d3bf479b
Suilong Luo <szl5645@psu.edu>
2017-08-06 15:41:22 -0400

    Update About page & Nav

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,9 @@ $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
+
+/*$('.dropdown-toggle').click(function() {
+    var location = $(this).attr('href');
+    window.location.href = location;
+    return false;
+});*/
