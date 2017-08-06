b680381de14244e171b881630e06fbc70a27635b
Suilong Luo <szl5645@psu.edu>
2017-08-06 19:41:22 -0400

    Updated Resume

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,3 @@ $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
-
-/*$('.dropdown-toggle').click(function() {
-    var location = $(this).attr('href');
-    window.location.href = location;
-    return false;
-});*/
